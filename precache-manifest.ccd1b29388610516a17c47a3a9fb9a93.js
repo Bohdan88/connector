0c1dfbcdedd7f08e6d5d38fc8505cdfd6e3dae35
bohdan88 <bohdan.martyniuk1488@gmail.com>
2020-05-19 10:55:22 -0400

    Updates

---
+++ precache-manifest.ccd1b29388610516a17c47a3a9fb9a93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41e2a9fd49e8a1016d26ab39dea0f01",
+    "url": "/connector/index.html"
+  },
+  {
+    "revision": "0d821eb52313d6f2bf99",
+    "url": "/connector/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "33cfcdd03d586f4e93fa",
+    "url": "/connector/static/css/main.f9632a23.chunk.css"
+  },
+  {
+    "revision": "0d821eb52313d6f2bf99",
+    "url": "/connector/static/js/2.d659f4ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/connector/static/js/2.d659f4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33cfcdd03d586f4e93fa",
+    "url": "/connector/static/js/main.1a1f5aa8.chunk.js"
+  },
+  {
+    "revision": "f408fa6dc6ba7878bbad",
+    "url": "/connector/static/js/runtime-main.f6f5c270.js"
+  },
+  {
+    "revision": "6edad9b199d074a78524dd2a8c932767",
+    "url": "/connector/static/media/toronto.6edad9b1.jpg"
+  }
+]);
